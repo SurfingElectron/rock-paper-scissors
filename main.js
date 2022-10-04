8f4278b1570dc2a2fe85be911835d9b57508a1ae
Emma <gill.emma.e@gmail.com>
2022-10-04 14:28:37 +0100

    Tidied up code, implemented the capital letter check.

--- main.js
+++ main.js
@@ -1,33 +1,50 @@
-//DEFINING OUR FUNCTIONS
+//INITIALISING VARIABLES
+const score = {roundCount: 0, winCount: 0, lossCount: 0, drawCount: 0};
 
-//checks a username, only accept if it starts with a letter which is a capital 
-//function checkUsername() {
-//    while (isLetter (nameCheck[0]) === false) {
-//        nameCheck = alert("Your name must start with a capital letter, please try again");
-//    } 
-//    return nameCheck;
-//}
+const rockButton = document.querySelector('#rock_button');
+const paperButton = document.querySelector('#paper_button');
+const scissorsButton = document.querySelector('#scissors_button');
 
-//handling the form submission
+const totalLi = document.querySelector('#total_games');
+const winLi = document.querySelector('#wins');
+const lossLi = document.querySelector('#losses');
+const drawLi = document.querySelector('#draws');
+
+const computerChoice = document.querySelector('#computer_choice');
+const playerChoice = document.querySelector('#player_choice');
+const results = document.querySelector('#results');
+const formDiv = document.querySelector('.username_form');
+const usernameForm = document.getElementById('username_entry');
+
+
+//EVENT LISTENERS
+rockButton.addEventListener('click', playRound);
+paperButton.addEventListener('click', playRound);
+scissorsButton.addEventListener('click', playRound);
+usernameForm.addEventListener('submit', handleSubmit);
+
+//FUNCTIONS
+//Form submission handler
 function handleSubmit(event) {
     event.preventDefault();
-  
     const data = new FormData(event.target);
     const username = data.get('username');
-  
-    console.log(username);
-    formDiv.style.visibility = 'hidden'; //hides the form div!
+    
+    if (!isLetter(username)) {
+        alert("Your name must start with a capital letter, please try again!");
+        return null;
+    }
+      
+    formDiv.style.visibility = 'hidden'; //hides the form after name entry
     playerChoice.innerText = `${username}, please choose your move:`
-    results.innerText = `${username}, your results:`
-  }
+}
   
-
-//check if the first character is a letter AND capital
+//Checks if the first character is a letter AND capital
 function isLetter(letterCheck) {
     return letterCheck !== letterCheck.toLowerCase();
 }
 
-//updating and displaying the scores
+//Score update and display
 function matchTracking(resultCheck) { 
     score.roundCount++;
     totalLi.innerText = `Games played: ${score.roundCount}`;
@@ -48,7 +65,7 @@ function matchTracking(resultCheck) {
     };
 }
 
-//respond with which RPS button was pushed
+//Player move announcer
 function assignPlayerMove(event){
     console.log(event.target.innerText);
     if (event.target.innerText == 'Rock') return "rock"
@@ -56,28 +73,28 @@ function assignPlayerMove(event){
     else if (event.target.innerText == 'Scissors')  return "scissors";
 }
 
-//generate the computer's choice and display it
+//Computer move generator and announcer
 function computerMove() {
   let computerRPS = '';  
   let randomNumber = Math.floor(Math.random() * (3 - 1 + 1)) + 1; //This should return 1, 2 or 3
     if(randomNumber === 1) {
         computerRPS = 'rock';
-        computerChoice.innerText = `The computer chose: ${computerRPS}`;
+        computerChoice.innerText = `The computer chose ${computerRPS}. Play again?`;
         return computerRPS;
     }
     else if(randomNumber === 2) {
         computerRPS = 'paper';
-        computerChoice.innerText = `The computer chose: ${computerRPS}`;
+        computerChoice.innerText = `The computer chose ${computerRPS}. Play again?`;
         return computerRPS;
     } 
     else if(randomNumber === 3) {
         computerRPS = 'scissors'
-        computerChoice.innerText = `The computer chose: ${computerRPS}`;
+        computerChoice.innerText = `The computer chose ${computerRPS}. Play again?`;
         return computerRPS;
     }
 }
 
-//logic check to see if player wins
+//Winning logic
 function getWinner(playerMove, computerMove) {
     if (playerMove === computerMove) {
         return 0;
@@ -105,36 +122,9 @@ function getWinner(playerMove, computerMove) {
     };
 };
 
-//run a round of RPS
+//Play RPS!
 function playRound(event) {
     let matchResult = getWinner(assignPlayerMove(event), computerMove());
     
     matchTracking (matchResult);
 }
-
-
-//INITIALISING VARIABLES
-const score = {roundCount: 0, winCount: 0, lossCount: 0, drawCount: 0};
-
-const rockButton = document.querySelector('#rock_button');
-const paperButton = document.querySelector('#paper_button');
-const scissorsButton = document.querySelector('#scissors_button');
-
-const totalLi = document.querySelector('#total_games');
-const winLi = document.querySelector('#wins');
-const lossLi = document.querySelector('#losses');
-const drawLi = document.querySelector('#draws');
-
-const computerChoice = document.querySelector('#computer_choice');
-const playerChoice = document.querySelector('#player_choice');
-const results = document.querySelector('#results');
-const formDiv = document.querySelector('.username_form');
-const usernameForm = document.getElementById('username_entry');
-
-
-//EVENT LISTENERS
-rockButton.addEventListener('click', playRound);
-paperButton.addEventListener('click', playRound);
-scissorsButton.addEventListener('click', playRound);
-usernameForm.addEventListener('submit', handleSubmit);
-
